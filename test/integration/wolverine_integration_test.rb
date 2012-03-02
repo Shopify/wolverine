a3203bdf2405f10c0ba566afb27e30b9317a701e
Burke Libbey <burke@burkelibbey.org>
2012-03-02 10:31:01 -0600

    Added integration test

---
+++ test/integration/wolverine_integration_test.rb
@@ -0,0 +1,25 @@
+require_relative '../test_helper'
+
+class WolverineIntegrationTest < MiniTest::Unit::TestCase
+
+  CONTENT = File.read(File.expand_path('../lua/util/mexists.lua', __FILE__))
+
+  def mock_redis
+    stub.tap do |redis|
+      redis.expects(:evalsha).
+        with('fe24f4dd4ba7881608cca4b846f009195e06d79a', 2, :a, :b).
+        raises("NOSCRIPT")
+      redis.expects(:eval).
+        with(CONTENT, 2, :a, :b).
+        returns([1, 0])
+    end
+  end
+
+  def test_everything
+    Wolverine.config.redis = mock_redis
+    Wolverine.config.script_path = Pathname.new(File.expand_path('../lua', __FILE__))
+
+    assert_equal [1, 0], Wolverine.util.mexists(:a, :b)
+  end
+
+end
