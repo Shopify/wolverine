9aa5246e859d3cc6a528bccf15200726736d1a45
Max Lahey <maxwellslahey@gmail.com>
2014-03-07 16:33:44 -0500

    spread out an overly complex line and added documentation to explain why the case exists

--- lib/wolverine.rb
+++ lib/wolverine.rb
@@ -71,7 +71,13 @@ class Wolverine
   end
 
   def method_missing sym, *args
-    if sym[0] == '_' then super else root_directory.send(sym, *args) end
+    # Disallow access to protected partials (files that begin with _ character)
+    if sym[0] == '_'
+      super
+    else
+      root_directory.send(sym, *args)
+    end
+    
   rescue PathComponent::MissingTemplate
     super
   end
